a20eb7072c7d11b307fda325d993789424415995
aframuz <aframuz@tutanota.io>
2022-04-20 20:54:19 -0400

    Added banner and form

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,13 +10,31 @@ window.addEventListener("resize", () => {
 
 // Set height of exercise column equal to description
 function exerciseHeightSetter() {
-   // Grab all description & exercises columns
+   // Grab all description & exercises columns, declare breakpoints
    const descriptionSections = document.querySelectorAll(".description")
    const exerciseSections = document.querySelectorAll(".exercise")
+   const breakpoints = {
+      sm: 576,
+      md: 768,
+      lg: 992,
+      xl: 1200,
+      xxl: 1400,
+   }
 
-   // Itereate through description sections, grab their height & apply them to corresponding exercise column, in px
-   descriptionSections.forEach((dSection, index) => {
-      const fixedHeight = dSection.children[0].offsetHeight
-      exerciseSections[index].children[0].style.height = `${fixedHeight}px`
-   })
+   // Change height if window width is above threshold, else remove height style attribute
+   if (window.innerWidth > breakpoints["lg"]) {
+      // Itereate through description sections, grab their height & apply them to corresponding exercise column, in px
+      descriptionSections.forEach((dSection, index) => {
+         const fixedHeight = dSection.children[0].offsetHeight
+         exerciseSections[index].children[0].style.height = `${fixedHeight}px`
+      })
+      return
+   } else {
+      exerciseSections.forEach((eSection) => {
+         if (eSection.children[0].getAttribute("style")) {
+            eSection.children[0].style.removeProperty("height")
+         }
+      })
+      return
+   }
 }
